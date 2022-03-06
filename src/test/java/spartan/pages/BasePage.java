a7df2f8d0f24d7fd3aabf4e8b26439600db98db7
OscarSah <oscar@cybertekschool.com>
2022-03-06 21:28:42 +0300

    POM Base Page added

---
+++ src/test/java/spartan/pages/BasePage.java
@@ -0,0 +1,10 @@
+package spartan.pages;
+
+import org.openqa.selenium.support.PageFactory;
+import spartan.utilities.Driver;
+
+public class BasePage {
+    public BasePage(){
+        PageFactory.initElements(Driver.get(),this);
+    }
+}
